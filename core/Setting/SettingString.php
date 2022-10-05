48f3fd0f7658bec5e77b26abe8099ab865b22893
Thomas Gollenia <thomas@gollenia.at>
2022-10-05 10:10:27 +0200

    first commitment

---
+++ core/Setting/SettingString.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace dokuwiki\plugin\config\core\Setting;
+
+/**
+ * Class setting_string
+ */
+class SettingString extends Setting
+{
+	/** @inheritdoc */
+	public function html(\admin_plugin_config $plugin, $echo = false)
+	{
+		$disable = '';
+
+		if ($this->isProtected()) {
+			$value = $this->protected;
+			$disable = 'disabled="disabled"';
+		} else {
+			if ($echo && $this->error) {
+				$value = $this->input;
+			} else {
+				$value = is_null($this->local) ? $this->default : $this->local;
+			}
+		}
+
+		$key = htmlspecialchars($this->key);
+		$value = htmlspecialchars($value);
+
+		$label = '<label for="config___' . $key . '">' . $this->prompt($plugin) . '</label>';
+		$input = '<div class="input-group"><input id="config___' . $key . '" name="config[' . $key .
+			']" type="text" class="form-control" value="' . $value . '" ' . $disable . '/></div>';
+		return array($label, $input);
+	}
+}
